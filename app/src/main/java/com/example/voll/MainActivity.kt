6789d9d2625dd981b0ec93d866b52166ca64f20c
Pavel <tr@tr.tr>
2023-09-19 17:45:36 +0300

    rec view

--- app/src/main/java/com/example/voll/MainActivity.kt
+++ app/src/main/java/com/example/voll/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.voll
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.UUID
+import java.util.concurrent.RecursiveAction
 import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
@@ -21,11 +29,44 @@ class Delegate{
     }
 }
 
+class OurRecyclerAdapter(val context: Context, val elements: MutableList<String>):
+    RecyclerView.Adapter<OurRecyclerAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val textView = view.findViewById<TextView>(R.id.uuid)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recy_item, parent, false)
+        val viewHolder = ViewHolder(view)
+        return viewHolder
+    }
+
+    fun update(new: String){
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return elements.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = elements[position]
+    }
+
+}
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
 
-    // lazy, observable
+        // lazy, observable
+
+    val ourList = mutableListOf<String>()
+
 
     var pablo: String by Delegate()
     val ppp: String by lazy{
@@ -38,9 +79,10 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    val vollViewModel : VollViewModel by viewModels(){
-        VollViewModelFactory(SavedStateHandle())
-    }
+    val vollViewModel : VollViewModel by viewModels()
+//    {
+//        VollViewModelFactory(SavedStateHandle())
+//    }
 
     lateinit var text: TextView
 
@@ -55,6 +97,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         text = findViewById(R.id.textView)
         updateText()
+        val recy = findViewById<RecyclerView>(R.id.recy)
+
+        ourList.addAll((1..30).map{UUID.randomUUID().toString()})
+
+        recy.layoutManager = LinearLayoutManager(this)
+        recy.adapter = OurRecyclerAdapter(this, ourList)
 
 
         pablo = "300"
--- app/src/main/java/com/example/voll/VollViewModel.kt
+++ app/src/main/java/com/example/voll/VollViewModel.kt
@@ -3,7 +3,7 @@ package com.example.voll
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class VollViewModel(val savedStateHandle: SavedStateHandle): ViewModel() {
+class VollViewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
 
     val strList = listOf("Who is the president of France?", "Who is the president of Japan?", "Who is the president of Germany?")
 
